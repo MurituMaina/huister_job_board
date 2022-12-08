4b95ce6464970481960ff12d786346d2ba1901c8
MurituMaina <vincent.maina@student.moringaschool.com>
2022-12-08 14:03:54 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react';
+import LoginForm from './navbar components/Login/LoginForm';
 import Navbar from './navbar components/navbar/Navbar';
 
 function App() {
+
+   const [recruiter, setRecruiter] = useState(null);
+
+   useEffect(() => {
+     // auto-login
+     fetch("/me").then((r) => {
+       if (r.ok) {
+         r.json().then((recruiter) => setRecruiter(recruiter));
+       }
+     });
+   }, []);
+
+   if (!recruiter) return <LoginForm onLogin={setRecruiter} />;
+
+
   return (
    <div className='App'>
     <Navbar />
+    {/* <LoginForm /> */}
+
    </div>
   );
 }
