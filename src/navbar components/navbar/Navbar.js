15c818a6c8e01e86dca4f89c5bbbd91ac3db8a3c
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-12-11 03:37:25 +0300

    minor style changes

--- src/navbar components/navbar/Navbar.js
+++ src/navbar components/navbar/Navbar.js
@@ -2,12 +2,12 @@ import "./navbar.css";
 import Home from "../home/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PostJob from "../Login/Login";
-// import ViewJobs from "../viewJobs/ViewJobs";
+import ViewJobs from "../viewJobs/ViewJobs";
 import AvailableJobs from "../AvailableJobs/AvailableJobs";
 import Jobs from "../jobs/Jobs";
 
 
-const Navbar = ({recruiter, setRecruiter}) => {
+const Navbar = ({ recruiter, setRecruiter }) => {
   // if
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
@@ -18,25 +18,26 @@ const Navbar = ({recruiter, setRecruiter}) => {
   }
   return (
     <header>
-    <Router>
-      <nav className="navbar">
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/Login">Login</Link>
-          {/* <Link to="/ViewJobs">VIEW JOBS</Link> */}
-          <Link to="/AvailableJobs">Available Jobs</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
-      </nav>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/Login" element={<PostJob />} />
-        {/* <Route path="/ViewJobs" element={<ViewJobs />} /> */}
-        <Route path="/AvailableJobs" element={<AvailableJobs />} />
-        <Route path="/Jobs" element={<Jobs />} />
-      </Routes>
-    </Router>
-    </header>
+      <Router>
+        <nav className="navbar">
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/ViewJobs">View Jobs</Link>
+            <Link to="/AvailableJobs">Available Jobs</Link>
+            <Link to="/jobs">Post a Job</Link>
+            <Link to="/Login">Login</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="/Login" element={ <PostJob /> } />
+          <Route path="/ViewJobs" element={ <ViewJobs /> } />
+          <Route path="/AvailableJobs" element={ <AvailableJobs /> } />
+          <Route path="/Jobs" element={ <Jobs /> } />
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </Router>    </header>
   );
 };
 
